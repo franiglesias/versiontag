ababa12d2d0ce22d2aa033c6f9e5385a4ed3f8c3
Patrik Kristian <patrik@pkristian.cz>
2018-08-03 01:02:55 +0200

    updated small things, tabs to spaces

--- testCases/testAll.sh
+++ testCases/testAll.sh
@@ -10,40 +10,41 @@ countFailed=0
 for testCase in $(ls -d */)
 do
 {
-	countTests=$((countTests + 1))
+    countTests=$((countTests + 1))
 
-	expected="$(cat $testCase/expected)"
+    expected="$(cat $testCase/expected)"
 
-	actual="$(bash $testCase/command.sh)"
+    actual="$(bash $testCase/command.sh)"
 
-	if [ "$expected" = "$actual" ]
-	then
-		printf "\e[32m"
-		printf '%s' "[ok] "
-		printf '%s\n' "${testCase/%?}";
-		continue 1;
-	fi
+    if [ "$expected" = "$actual" ]
+    then
+        printf "\e[32m"
+        printf '%s' "[ok] "
+        printf "\e[0m";
+        printf '%s\n' "${testCase/%?}";
+        continue 1;
+    fi
 
-	countFailed=$((countFailed + 1))
+    countFailed=$((countFailed + 1))
 
-	printf "\e[31m"
-	printf "[error] "
+    printf "\e[31m"
+    printf "[error] "
 
-	printf "\e[0m";
-	printf '%s\n' "${testCase/%?}";
+    printf "\e[0m";
+    printf '%s\n' "${testCase/%?}";
 
-	printf "\e[96m"
-	printf '%s\n' "--expected:"
+    printf "\e[96m"
+    printf '%s\n' "--expected:"
 
-	printf "\e[92m"
-	echo "$expected"
+    printf "\e[92m"
+    echo "$expected"
 
-	printf "\e[96m"
-	printf '%s\n' "--actual:"
+    printf "\e[96m"
+    printf '%s\n' "--actual:"
 
-	printf "\e[35m"
-	echo "$actual"
-	printf "\e[0m"
+    printf "\e[35m"
+    echo "$actual"
+    printf "\e[0m"
 }
 done
 
@@ -52,13 +53,13 @@ printf -- "\n"
 if [ $countFailed -eq 0 ]
 then
 {
-	printf "\e[32m"
-	printf '%s\n' "Passed all $countTests tests."
+    printf "\e[32m"
+    printf '%s\n' "Passed all $countTests tests."
 }
 else
 {
-	printf "\e[31m"
-	printf '%s\n' "Failed $countFailed from $countTests tests."
+    printf "\e[31m"
+    printf '%s\n' "Failed $countFailed from $countTests tests."
 }
 fi
 
