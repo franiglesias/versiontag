0207e579442c4d8726f54b9ff6c48a08011ec392
Fran Iglesias <franiglesias@mac.com>
2018-08-07 22:04:24 +0200

    More tests

---
+++ testCases/minorWithMessage/command.sh
@@ -0,0 +1,12 @@
+cd ../testRepo
+git tag v1.1.1 > /dev/null 2>&1
+
+bash ../versiontag \
+    --force \
+    minor --message 'tag message' \
+    | grep -v "\[test" #ignore line with commit message
+
+git tag -d v1.1.1 > /dev/null 2>&1
+git push origin :v1.1.1 > /dev/null 2>&1 || true
+git tag -d v1.2.0 > /dev/null 2>&1
+git push origin :v1.2.0 > /dev/null 2>&1 || true
---
+++ testCases/patchWithMessage/command.sh
@@ -0,0 +1,12 @@
+cd ../testRepo
+git tag v1.1.1 > /dev/null 2>&1
+
+bash ../versiontag \
+    --force \
+    patch -m 'tag message'\
+    | grep -v "\[test" #ignore line with commit message
+
+git tag -d v1.1.1 > /dev/null 2>&1
+git push origin :v1.1.1 > /dev/null 2>&1 || true
+git tag -d v1.1.2 > /dev/null 2>&1
+git push origin :v1.1.2 > /dev/null 2>&1 || true
